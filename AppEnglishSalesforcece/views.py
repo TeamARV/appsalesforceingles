86cd4a195fe708b3c3540c93cddf23cc5422af95
Andres <andresriveravega@msn.com>
2023-07-28 19:15:48 -0500

    update conexion ok and landing page

--- AppEnglishSalesforcece/views.py
+++ AppEnglishSalesforcece/views.py
@@ -14,29 +14,69 @@ def index(request):
 
 
 from simple_salesforce import Salesforce
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+
+
 
 def testf1(request):
-    # Configura los detalles de conexión con Salesforce
-    username = 'riveravegaandres@wise-hawk-b8wjs3.com'
-    password = '+pastdostrescuatroX59'
-    security_token = '3MVG9g9rbsTkKnAXRU_hOLvHRYnx8qJ_AO2DpfdGUKxIj8wClK1IGCwGRFR0ZMc4qtjH5tgr2EGMdobzpa7_G'
-    #sandbox = False  # Establecer en True si estás usando un sandbox de Salesforce
 
-    # Crea una instancia del cliente Salesforce
-    sf = Salesforce(username=username, password=password, security_token=security_token)
 
-    # Ahora puedes utilizar los métodos de la instancia "sf" para interactuar con Salesforce,
-    # como consultar datos, crear registros, actualizar registros, etc.
 
-    # Ejemplo: Consultar algunos registros de Salesforce
-    consulta_resultados = sf.query("SELECT Id, Name, Email FROM Contact LIMIT 5")
+    CONSUMER_KEY = '3MVG99gP.VbJma8UueUneeXfQ6jjTlMjJX1Xsp8k7u.eMJX27iWaltPgaM3oJI5sqGdsJcdj8u6ZZMtGCmgYu'
+    CONSUMER_SECRET= 'E930064C14C5E307B8AD6229F742C184E15D16CA17EBEA0A134E540DC17E3BB3'
+    USERNAME ='riveravegaandres@wise-hawk-b8wjs3.com'
+    PASSWORD ='+pastdostrescuatroX939'
+    DOMAIN = 'https://login.salesforce.com'
+    DOMAIN_NAME = DOMAIN
+    DOMAIN_NAME2='https://wise-hawk-b8wjs3-dev-ed.trailblaze.my.salesforce.com'
 
-    # El resultado se encuentra en consulta_resultados['records']
+    json_data = {
+    'grant_type': 'password',
+    'username': USERNAME,
+    'password': PASSWORD,
+    'client_id': CONSUMER_KEY,
+    'client_secret': CONSUMER_SECRET,
+    'content-type': 'application/json'
+}
 
-    # Puedes pasar los resultados a tu template para mostrarlos en tu vista HTML
-    context = {
-        'resultados': consulta_resultados['records'],
-    }
 
-    return render(request, 'testf1.html', context)
+    
+
+    uri_token_request = DOMAIN_NAME + '/services/oauth2/token'
+    response = requests.post(uri_token_request, data=json_data)
+    print( response.json())
+    access_token = response.json()['access_token']
+    print('el token access --->' + access_token)
+
+    url = 'https://wise-hawk-b8wjs3-dev-ed.trailblaze.my.salesforce.com/services/data/v52.0/limits'
+
+    headers = {
+	'Authorization': 'Bearer ' + access_token,
+    'Content-Type': 'application/json'
+}
+    response1 = requests.get(DOMAIN_NAME2 + '/services/data/v53.0/query/?q=SELECT+Id,CreatedById,Name,WordES__c,WordIN__c+FROM+Diccionario__c limit 10', headers=headers)
+    print(response.json())
+
+    response1 = response1.json()
+
+    # Realizar la solicitud GET a la API de límites
+    response = requests.get(url, headers=headers)
+
+    # Obtener la respuesta en formato JSON
+    limits_data = response.json()
+
+    #Imprimir los límites y el uso actual
+    for limit_name, limit_value in limits_data.items():
+        print(f'{limit_name}: {limit_value["Max"]} (usado: {limit_value["Remaining"]})')
+
+
+
+
+
+
+    return render(request, 'testf1.html', {'response': response1})
+
+
 
